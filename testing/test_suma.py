1eb51e59e7bd2439b4cb6d84d32dea80972fa502
Esau Oliva <152229668+EsauOlivaLeaoHelix@users.noreply.github.com>
2024-10-02 20:05:33 -0600

    Create test_suma.py

---
+++ testing/test_suma.py
@@ -0,0 +1,14 @@
+import unittest
+
+class TestSuma(unittest.TestCase):
+    def test_suma_positivos(self):
+        self.assertEqual(suma(1, 2), 3)
+
+    def test_suma_negativos(self):
+        self.assertEqual(suma(-1, -2), -3)
+
+    def test_suma_cero(self):
+        self.assertEqual(suma(0, 0), 0)
+
+if __name__ == '__main__':
+    unittest.main()
